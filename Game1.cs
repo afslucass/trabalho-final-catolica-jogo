56dcea6b4c4ac674d07a78f97a87fd146c73dda4
Lucas Custodio <afslucass@gmail.com>
2026-08-24 20:18:14 -0300

    feat: add sprite

--- Game1.cs
+++ Game1.cs
@@ -2,11 +2,18 @@
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using GameCore;
+using GameCore.Graphics;
 
 namespace jogo;
 
 public class Game1 : Core
 {
+    // texture region that defines the slime sprite in the atlas.
+    private TextureRegion _slime;
+
+    // texture region that defines the bat sprite in the atlas.
+    private TextureRegion _bat;
+
     public Game1() : base("Jogo", 1920, 1080, true)
     {
 
@@ -20,10 +27,15 @@ public class Game1 : Core
     }
 
     protected override void LoadContent()
-    {
-        // TODO: use this.Content to load your game content here
-        
-        base.LoadContent();
+    {   
+        // Create the texture atlas from the XML configuration file
+        TextureAtlas atlas = TextureAtlas.FromFile(Content, "images/atlas-definition.xml");
+
+        // retrieve the slime region from the atlas.
+        _slime = atlas.GetRegion("slime");
+
+        // retrieve the bat region from the atlas.
+        _bat = atlas.GetRegion("bat");
     }
 
     protected override void Update(GameTime gameTime)
@@ -40,7 +52,17 @@ public class Game1 : Core
     {
         GraphicsDevice.Clear(Color.CornflowerBlue);
 
-        // TODO: Add your drawing code here
+        // Begin the sprite batch to prepare for rendering.
+        SpriteBatch.Begin(samplerState: SamplerState.PointClamp);
+
+        // Draw the slime texture region at a scale of 4.0
+        _slime.Draw(SpriteBatch, Vector2.Zero, Color.White, 0.0f, Vector2.One, 4.0f, SpriteEffects.None, 0.0f);
+
+        // Draw the bat texture region 10px to the right of the slime at a scale of 4.0
+        _bat.Draw(SpriteBatch, new Vector2(_slime.Width * 4.0f + 10, 0), Color.White, 0.0f, Vector2.One, 4.0f, SpriteEffects.None, 1.0f);
+
+        // Always end the sprite batch when finished.
+        SpriteBatch.End();
 
         base.Draw(gameTime);
     }
