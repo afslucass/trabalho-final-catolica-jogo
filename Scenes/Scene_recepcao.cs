f780f564d065738a331f7f07b8133d0f2c0bc5b2
ARTHUR FARIA DOS SANTOS <af096396@gmail.com>
2026-09-13 17:20:27 -0300

    feat: implement basic rendering and camera in Scene_recepcao

--- Scenes/Scene_recepcao.cs
+++ Scenes/Scene_recepcao.cs
@@ -1,32 +1,52 @@
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using GameCore;
+using GameCore.Camera;
+using GameCore.Graphics;
 using GameCore.Scenes;
 
 namespace TestGame.Scenes;
 
 public class Scene_recepcao : Scene
 {
+    private Camera2D _camera;
+    private AnimatedSprite _slime;
+    private Vector2 _slimePosition;
+
     public override void Initialize()
     {
         base.Initialize();
+        _camera = new Camera2D();
+        _slimePosition = new Vector2(100, 100); // Posição inicial nesta nova sala
     }
 
     public override void LoadContent()
     {
         base.LoadContent();
+
+        TextureAtlas atlas = TextureAtlas.FromFile(Content, "images/atlas-definition.xml");
+        
+
+        _slime = atlas.CreateAnimatedSprite("slime-animation");
+        _slime.Scale = new Vector2(4.0f, 4.0f);
     }
 
     public override void Update(GameTime gameTime)
     {
         base.Update(gameTime);
+
+        _slime.Update(gameTime);
+        _camera.Follow(_slimePosition, Core.GraphicsDevice.Viewport);
+        
     }
 
     public override void Draw(GameTime gameTime)
     {
-        // Limpa a tela com uma cor diferente (ex: preto ou verde escuro)
-        // para confirmar visualmente que a cena mudou
-        Core.GraphicsDevice.Clear(Color.White);
+        Core.GraphicsDevice.Clear(Color.DarkSlateGray);
+
+        Core.SpriteBatch.Begin(samplerState: SamplerState.PointClamp, transformMatrix: _camera.TransformMatrix);
+        _slime.Draw(Core.SpriteBatch, _slimePosition);
+        Core.SpriteBatch.End();
 
         base.Draw(gameTime);
     }
